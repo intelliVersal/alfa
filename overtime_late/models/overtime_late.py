15801b2783e7362e28893ad43afc7cf34b22c520
umair0099 <umairsabir674@gmail,com>
2021-10-13 18:27:14 +0300

    []

--- overtime_late/models/overtime_late.py
+++ overtime_late/models/overtime_late.py
@@ -921,9 +921,13 @@ class Employee_report(models.Model):
                 ('date', '>=', __(self.overtime_date_from)),
                 ('date', '<=', __(self.overtime_date_to)),
             ])
-            self.total_overtime = sum([att.overtime_assigned_hours for att in overtime_days if att.overtime_assignment_id.state == 'confirmed'])
-            self.total_overtime_amount = sum(
-                [att.overtime_assignment_id.overtime_calc for att in overtime_days if att.overtime_assignment_id.state == 'confirmed'])
+            total_overtime = 0
+            for att_over in overtime_days:
+                total_overtime += att_over.overtime_minutes
+            self.total_overtime = total_overtime
+            # self.total_overtime = sum([att.overtime_assigned_hours for att in overtime_days if att.overtime_assignment_id.state == 'confirmed'])
+            # self.total_overtime_amount = sum(
+            #     [att.overtime_assignment_id.overtime_calc for att in overtime_days if att.overtime_assignment_id.state == 'confirmed'])
             self.overtime_day_ids = overtime_days.ids
         self.total_delay_round = round(self.total_delay, 2)
         self.total_overtime_round = round(self.total_overtime, 2)
