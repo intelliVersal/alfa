72586cfb76704a95cf9c483e84f24020be6e156e
umair0099 <umairsabir674@gmail,com>
2021-10-13 16:54:49 +0300

    []

--- hr_customizations/__manifest__.py
+++ hr_customizations/__manifest__.py
@@ -7,13 +7,13 @@
     'category': 'Custom',
     'version': '12.0.1',
 
-    'depends': ['base','hr','saudi_hr_employee','saudi_hr_leaves','overtime_late','saudi_hr_payroll','basic_hr','saudi_hr_eos','sale_subscription','account'],
+    'depends': ['base','hr','saudi_hr_employee','saudi_hr_leaves','overtime_late','saudi_hr_payroll','basic_hr','saudi_hr_eos','account'],
 
     'data': [
         'security/ir.model.access.csv',
         'security/security.xml',
         'wizard/wizard_view.xml',
-        'wizard/payslip_report_view.xml',
+        # 'wizard/payslip_report_view.xml',
         'views/model_view.xml',
         'views/cron_view.xml'
             ],
