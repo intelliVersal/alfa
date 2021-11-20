9c412a214772954960d062600d423a32bb816cb7
umair0099 <umairsabir674@gmail,com>
2021-11-20 15:53:29 +0300

    [update sale confirmation scenrio]

--- sale_restrictions/model/credit_day_limit.py
+++ sale_restrictions/model/credit_day_limit.py
@@ -7,12 +7,6 @@ from odoo.exceptions import Warning, UserError, ValidationError
 class SaleOrderInherit(models.Model):
     _inherit = 'sale.order'
 
-    def check_discount_rate(self):
-        for rec in self.order_line:
-            if not self.env.user.has_group('sale_restrictions.allow_maximum_discount'):
-                if rec.discount >= 5.0:
-                    raise ValidationError(_('You are not allowed to give %s or more than %s discount')%(rec.discount,rec.discount))
-
     def get_confirm_so_amount(self):
         if not self.env.user.has_group('sale_restrictions.allow_credit_sale'):
             self.env.cr.execute("""
@@ -24,13 +18,33 @@ class SaleOrderInherit(models.Model):
             amount = self.env.cr.fetchall()
             if not amount[0][0]:
                 return 0
-            due_amount = amount[0][0] - self.amount_total
+            due_amount = amount[0][0]
             if due_amount > self.partner_id.credit_limit:
-                raise ValidationError(_('Sale Order confirmation is not possible, because outstanding balance of this customer is %s, and credit limit of this customer is %s') % (amount[0][0], self.partner_id.credit_limit))
+                raise ValidationError(_('Sale Order confirmation is not possible, because outstanding balance of this customer is %s, and credit limit of this customer is %s or sale orders of this customer are confirmed but not invoiced yet.') % (amount[0][0], self.partner_id.credit_limit))
+
+    def get_payment_current_so(self):
+        if self.partner_id.allow_credit_sale == False:
+            payment_obj = self.env['account.payment'].sudo().search([('partner_id','=',self.partner_id.id),('state','=','posted'),('sale_order_id','=',self.id)])
+            if not payment_obj:
+                raise ValidationError(_('Kindly, Enter the payment against this sale order, credit sale is not allowed to this customer'))
+            else:
+                payment_amount = 0.0
+                for rec in payment_obj:
+                    payment_amount += rec.amount
+                if payment_amount < ((self.amount_total * 50)/100):
+                    raise ValidationError(_('Payment amount should must be 50% of the total amount, kindly check the payment against this sale order'))
+        else:
+            self.get_confirm_so_amount()
+
+    def check_discount_rate(self):
+        for rec in self.order_line:
+            if not self.env.user.has_group('sale_restrictions.allow_maximum_discount'):
+                if rec.discount >= 5.0:
+                    raise ValidationError(_('You are not allowed to give %s or more than %s discount')%(rec.discount,rec.discount))
 
     @api.multi
     def action_confirm(self):
         self.check_discount_rate()
-        self.get_confirm_so_amount()
-        res = super(SaleOrderInherit,self).action_confirm()
+        self.get_payment_current_so()
+        res = super(SaleOrderInherit, self).action_confirm()
         return res
