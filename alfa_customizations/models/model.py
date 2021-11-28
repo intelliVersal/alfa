b10e6b4caeca502ccab8a5027a4884cfbf165f5f
umair0099 <umairsabir674@gmail,com>
2021-11-28 17:26:15 +0300

    []

--- alfa_customizations/models/model.py
+++ alfa_customizations/models/model.py
@@ -16,16 +16,16 @@ class InheritWarehouse(models.Model):
 class SaleInherit(models.Model):
     _inherit = 'sale.order'
 
-    @api.model
-    def _default_warehouse_id(self):
-        company = self.env.user.company_id.id
-        warehouse_ids = self.env['stock.warehouse'].search([('company_id','=', company)], limit=1)
-        return warehouse_ids
-
-    warehouse_id = fields.Many2one(
-        'stock.warehouse', string='Warehouse',
-        required=True, readonly=True, states={'draft': [('readonly', False)], 'sent': [('readonly', False)]}, domain="[('is_raw_location','=',False)]",
-        default=_default_warehouse_id)
+    # @api.model
+    # def _default_warehouse_id(self):
+    #     company = self.env.user.company_id.id
+    #     warehouse_ids = self.env['stock.warehouse'].search([('company_id','=', company)], limit=1)
+    #     return warehouse_ids
+    #
+    # warehouse_id = fields.Many2one(
+    #     'stock.warehouse', string='Warehouse',
+    #     required=True, readonly=True, states={'draft': [('readonly', False)], 'sent': [('readonly', False)]}, domain="[('is_raw_location','=',False)]",
+    #     default=_default_warehouse_id)
 
     order_type = fields.Selection([('local','Local'),('export','Export')], default='local')
 
